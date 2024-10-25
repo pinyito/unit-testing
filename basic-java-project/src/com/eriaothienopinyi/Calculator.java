8c2cb1899aae6c593116b71184ee99429752ebb7
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2024-10-25 03:58:07 -0500

    Implemented basics of JUnit testing

--- basic-java-project/src/com/eriaothienopinyi/Calculator.java
+++ basic-java-project/src/com/eriaothienopinyi/Calculator.java
@@ -2,6 +2,10 @@ package com.eriaothienopinyi;
 
 public class Calculator {
     public int integerDivision(int dividend, int divisor){
-        return 0;
+        return dividend / divisor;
+    }
+
+    public int integerSubtraction(int num1, int num2){
+        return num1 - num2;
     }
 }
--- basic-java-project/test/com/eriaothienopinyi/CalculatorTest.java
+++ basic-java-project/test/com/eriaothienopinyi/CalculatorTest.java
@@ -1,12 +1,60 @@
 package com.eriaothienopinyi;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test Math Operations in Calculator Class")
 class CalculatorTest {
 
-    @Test
-    void integerDivision() {
+    Calculator calculator;
+
+    @BeforeAll
+    static void declareCalculatorVariable(){
+        System.out.println("Some logic here! Performed before test starts");
     }
+
+    @BeforeEach
+    void createCalculatorInstance(){
+        System.out.println("Before each test method is executed...");
+        calculator = new Calculator();
+    }
+
+    @DisplayName("Test 4/2=2")
+    @Test
+    void integerDivisionTest() {
+
+        assertEquals(2, calculator.integerDivision(4, 2), "4/2 did not yield 2!");
+    }
+
+    @DisplayName("Test 4-2=2")
+    @Test
+    void integerSubtractionTest(){
+        assertEquals(2, calculator.integerSubtraction(4, 2), () -> "4-2 does not yield 2!");
+    }
+
+    //Naming Tests:: test<system under test>_<condition or state change>_expected result>
+    //@Disabled("TO DO: Still figuring out the cause of failure.")
+    @DisplayName("Division by zero")
+    @Test
+    void testIntegerDivision_WhenDividendIsDividedByZero_ShouldThrowArithmeticException(){
+        //Arrange
+        int dividend = 4;
+        int divisor = 0;
+
+        //Act && Assert
+        assertThrows(ArithmeticException.class, () -> {
+            // Act
+            calculator.integerDivision(dividend, divisor);
+        }, "Division by zero should have thrown an exception");
+    }
+
+    // test method structure
+    /*
+     *  Arrange OR Given
+     *
+     *  Act OR When
+     *
+     *  Assert OR Then
+     */
 }
